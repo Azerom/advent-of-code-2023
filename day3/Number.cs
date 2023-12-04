1c51c5c8b62189cdf48e51d9255f1eb5dd461d47
Azerom <10066920+Azerom@users.noreply.github.com>
2023-12-04 01:00:16 +0100

    feat: Day 3

---
+++ day3/Number.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3
+{
+    internal class Number
+    {
+        public int Y { get; set; }
+
+        public int StartX { get; set; }
+
+        public int EndX { get; set; }
+
+        public int Value { get; set; }
+
+        /// <summary>
+        /// Represent the result of a scan of adjacent lines by a Number
+        /// The gears ids may be part of an incomplete gear (as in with only one element)
+        /// </summary>
+        /// <param name="HasAdjacentSymbol"></param>
+        /// <param name="GearsIds"></param>
+        public record ScanReport (bool HasAdjacentSymbol, IEnumerable<string> GearsIds);
+
+        /// <summary>
+        /// Scan adjacent lines to find symbol and/or potential gear
+        /// Doesn't check if found gear are connected to two number, only search for posible gear
+        /// </summary>
+        /// <param name="closeLines"></param>
+        /// <returns></returns>
+        public ScanReport ScanLines(IEnumerable<Line> closeLines)
+        {
+            bool hasSymbol = false;
+            List<string> gears = [];
+
+
+            foreach (Line line in closeLines)
+            {
+
+                if (line.Y == Y)
+                {
+                    //As we are scanning the line where the number is present, we only need to check the char before and the one after
+                    char? previousChar = StartX > 0 ? line.Value[StartX - 1] : null;
+                    char? nextChar = EndX < line.XMax ? line.Value[EndX + 1] : null;
+
+                    if (previousChar != null && IsSymbol(previousChar.Value))
+                    {
+                        hasSymbol = true;
+                        if (previousChar == '*')
+                        {
+                            gears.Add($"{line.Y}-{StartX - 1}");
+                        }
+                    }
+
+                    if (nextChar != null && IsSymbol(nextChar.Value))
+                    {
+                        hasSymbol = true;
+                        if (nextChar == '*')
+                        {
+                            gears.Add($"{line.Y}-{EndX + 1}");
+                        }
+                    }
+                }
+                else if( line.Y == Y + 1 || line.Y == Y - 1)
+                {
+                    //General case : we need to get a substring containing only the characters adjacent to the number and check them
+                    var minX = StartX > 0 ? StartX - 1 : StartX;
+                    var maxX = EndX < line.XMax ? EndX + 1 : EndX;
+                    var lenght = maxX - minX;
+                    var inspectedSub = line.Value.Substring(minX, lenght + 1);
+
+                    for (int i = 0; i < inspectedSub.Length; i++)
+                    {
+                        if (IsSymbol(inspectedSub[i]))
+                        {
+                            hasSymbol = true;
+                            if (inspectedSub[i] == '*')
+                            {
+                                gears.Add($"{line.Y}-{minX + i}");
+                            }
+                        }
+                    }
+                }
+            }
+
+            return new ScanReport(hasSymbol, gears);
+        }
+
+        private static bool IsSymbol(char c)
+        {
+            return !char.IsDigit(c) && c != '.';
+        }
+    }
+}
---
+++ day3/Program.cs
@@ -0,0 +1,89 @@
+﻿using day3;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+internal record Line(string Value, int Y, int XMax);
+
+internal record Gear(string Id, List<Number> Elements)
+{
+    public int Ratio => Elements.Select(e => e.Value).Aggregate((p, e) => p * e);
+}
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const string regex = @"(?:\D*(?'num'\d+)\D*)+";
+
+        try
+        {
+            using var sr = new StreamReader("input.txt");
+
+            List<Line> lines = [];
+            string? line;
+            int y = 0;
+            List<Number> numbers = [];
+            while ((line = sr.ReadLine()) != null)
+            {
+                lines.Add(new Line(line, y, line.Length - 1));
+
+                var match = Regex.Match(line, regex);
+                numbers.AddRange(match.Groups["num"].Captures.Cast<Capture>()
+                    .Select(number =>
+                    {
+                        return new Number { Value = int.Parse(number.Value), Y = y, StartX = number.Index, EndX = number.Index + number.Length - 1 };
+                    }));
+                y++;
+            }
+
+            List<Gear> potentialGears = [];
+
+            IEnumerable<Number> elements = numbers.Where(number =>
+            {
+                //Find lines close to the inspected number to scan them
+                var closeLines = new List<Line>();
+                if (number.Y > 0)
+                {
+                    closeLines.Add(lines[number.Y - 1]);
+                }
+                closeLines.Add(lines[number.Y]);
+                if (number.Y < lines.Count - 1)
+                {
+                    closeLines.Add(lines[number.Y + 1]);
+                }
+
+                var report = number.ScanLines(closeLines);
+
+                //Check if found possible gears are already reported and add the Number to their list
+                //or create a new one if this is the first time we find this gear
+                foreach (var gearID in report.GearsIds)
+                {
+                    var cGear = potentialGears.FirstOrDefault(g => g.Id == gearID);
+
+                    if (cGear != null)
+                    {
+                        cGear.Elements.Add(number);
+                    }
+                    else
+                    {
+                        potentialGears.Add(new Gear(gearID, [number]));
+                    }
+                }
+
+                return report.HasAdjacentSymbol;
+            });
+
+            IEnumerable<Gear> completeGears = potentialGears.Where(g => g.Elements.Count > 1);
+
+            Console.WriteLine($"Sum is {elements.Sum(e => e.Value)}");
+            Console.WriteLine($"Sum of gears ratio is {completeGears.Sum(e => e.Ratio)}");
+
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("Input could not be read:");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
